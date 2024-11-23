dd2aec8514545efa70b064c647ed19a27bbac3d6
gdegjygr <malcevartem808@gmail.com>
2024-11-23 15:36:56 +0600

    finished tuning tier-list page

--- src/app/pages/tier-list/page.jsx
+++ src/app/pages/tier-list/page.jsx
@@ -1,11 +1,16 @@
+'use client'
 import { TierListModule } from '@/modules/TierListModule/TierListModule'
 import styles from './tier-list.module.scss'
+import { TopBanner } from '@/UI/components/TopBanner/TopBanner'
+import { UnderBanner } from '@/UI/components/UnderBanner/UnderBanner'
 
 export const TierList = () => {
   return (
     <section>
       <main className={styles.tierListPage}>
+        <TopBanner />
         <TierListModule />
+        <UnderBanner />
       </main>
     </section>
   )
--- src/modules/TierListModule/TierListModule.jsx
+++ src/modules/TierListModule/TierListModule.jsx
@@ -4,6 +4,7 @@ import styles from './TierListModule.module.scss'
 import { useTierHeroes } from './TierListApi/TierListApi'
 import Image from 'next/image'
 import { useEffect } from 'react'
+import logo from '@/app/assets/icons/tierLogo.svg'
 
 export const TierListModule = () => {
   const { tierList, tierRequest } = useTierHeroes()
@@ -13,7 +14,7 @@ export const TierListModule = () => {
   }, [tierRequest])
 
   const dragStart = (e, id) => {
-    e.dataTransfer.setData('text/plain', `hero-${id}`)
+    e.dataTransfer.setData('text/plain', `id-${id}`)
   }
 
   const handleDragOver = (e) => {
@@ -24,50 +25,100 @@ export const TierListModule = () => {
     e.preventDefault()
     const data = e.dataTransfer.getData('text/plain')
     const draggedElement = document.querySelector(`.${data}`)
-    if (draggedElement) {
+    if (
+      (draggedElement && e.target.classList.contains(styles.sectionBlock)) ||
+      e.target.classList.contains(styles.allHeroesContainer)
+    ) {
       e.target.appendChild(draggedElement)
     }
   }
 
   return (
-    <section>
-      <div className={styles.firstSection}>
-        <div className={styles.sectionType}>
-          <Typography variant='h1'>S</Typography>
+    <section className={styles.tierSection}>
+      <section>
+        <article className={styles.sectionContainer}>
+          <div className={styles.typeS}>
+            <Typography variant='h1' className={styles.sectionTitle}>
+              S
+            </Typography>
+          </div>
+          <div
+            className={styles.sectionBlock}
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+          />
+        </article>
+        <article className={styles.sectionContainer}>
+          <div className={styles.typeA}>
+            <Typography variant='h1' className={styles.sectionTitle}>
+              a
+            </Typography>
+          </div>
+          <div
+            className={styles.sectionBlock}
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+          />
+        </article>
+        <article className={styles.sectionContainer}>
+          <div className={styles.typeB}>
+            <Typography variant='h1' className={styles.sectionTitle}>
+              b
+            </Typography>
+          </div>
+          <div
+            className={styles.sectionBlock}
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+          />
+        </article>
+        <article className={styles.sectionContainer}>
+          <div className={styles.typeC}>
+            <Typography variant='h1' className={styles.sectionTitle}>
+              c
+            </Typography>
+          </div>
+          <div
+            className={styles.sectionBlock}
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+          />
+        </article>
+        <article className={styles.sectionContainer}>
+          <div className={styles.typeD}>
+            <Typography variant='h1' className={styles.sectionTitle}>
+              d
+            </Typography>
+          </div>
+          <div
+            className={styles.sectionBlock}
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+          />
+        </article>
+      </section>
+      <aside
+        className={styles.allHeroesContainer}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+      >
+        {tierList.map((item) => (
+          <Image
+            key={item.id}
+            id={`id-${item.id}`}
+            className={`${styles.hero} id-${item.id}`}
+            draggable
+            onDragStart={(e) => dragStart(e, item.id)}
+            src={item.avatar}
+            alt='hero'
+            width={70}
+            height={70}
+          />
+        ))}
+        <div className={styles.logo}>
+          <Image src={logo} alt='logo' width={70} height={70} />
         </div>
-        <div
-          className={styles.sectionBlock}
-          onDragOver={handleDragOver}
-          onDrop={handleDrop}
-        />
-      </div>
-      {tierList.map((item) => (
-        <Image
-          key={item.id}
-          id={`hero-${item.id}`}
-          className={`${styles.hero} hero-${item.id}`}
-          draggable
-          onDragStart={(e) => dragStart(e, item.id)}
-          src={item.avatar}
-          alt='hero'
-          width={70}
-          height={70}
-        />
-      ))}
+      </aside>
     </section>
   )
 }
-
-// export const TierListModule = () => {
-//   return (
-//     <section>
-//       <div className={styles.firstSection}>
-//         <div className={styles.sectionType}>
-//           <Typography variant='h1'>S</Typography>
-//         </div>
-//         <div className={styles.sectionBlock} />
-//       </div>
-//       <div className={styles.hero} />
-//     </section>
-//   )
-// }
